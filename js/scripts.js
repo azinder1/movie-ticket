8279a1c4f61923435519c9141cc8f6e526117129
Adam Zinder and Eric Bomblatus <adam@>
2016-11-01 14:58:46 -0700

    adds prototype that can copute price

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
-function Movie (movie, age, time) {
-  this.newTitle = movie;
-  this.newAge = age;
-  this.newTime = time;
-  this.newPrice = [];
+function Movie (title, age, time) {
+  // debugger;
+  this.titles = title;
+  this.ages = age;
+  this.times = time;
+  this.price = 0;
 }
+
+Movie.prototype.Ticket = function(newMovie) {
+  // debugger;
+  var price = 14;
+  if (this.titles === "old") {
+    price *= 0.8;
+  }
+  if (this.ages >= 12 || this.ages <= 65) {
+    price *= 0.8;
+  }
+  if (this.times < 18) {
+    price *= 0.8;
+  }
+  return price;
+}
+
 
 $(document).ready(function() {
   $('form#movies').submit(function(event){
     event.preventDefault();
-    var inputtedTime = parseInt($("input:radio[name=time]").val());
+    var inputtedMovie = ($("#movie-titles").val());
+    var inputtedTime = parseInt($("input:radio[name=time]:checked").val());
     var inputtedAge = $("input#new-age").val();
-    var inputtedMovie = $("#movie-titles").val();
-    var newMovie = new Movie(inputtedMovie, inputtedAge, inputtedTime)
+    var newMovie = new Movie(inputtedMovie, inputtedAge, inputtedTime);
+    newMovie.price = newMovie.Ticket(newMovie);
+
+  $("#output").show();
+  $("#outputJumbo").show();
+  $("h2#confirm").show();
+
+   $("ul#your-movie").append("<li><span class='movie-hide'>" + newMovie.titles + "</span></li>");
+
+  $("input#movie-titles").val("");
+  $("input#time").val("");
+  $("input#new-age").val("");
+  $(".output").last().click(function() {
+    $("#your-movie").show();
+    $(".new-time").text(newMovie.times);
+    $(".price").text(newMovie.price);
+  });
   });
 });
